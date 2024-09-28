02cfa1c4566b359107c44bfb2a47593a43f513b8
damianludwiczak89 <damian_konin@tlen.pl>
2024-09-28 16:59:41 +0200

    refactored embarrassing piece of code

--- fit/helpers.py
+++ fit/helpers.py
@@ -1,43 +1,5 @@
 # Function for calendar index (placement)
 def cal_index(day, time):
-    # Define a number for each day of the week for calculation
-    match day:
-        case "Monday":
-            day = 0
-        case "Tuesday":
-            day = 10
-        case "Wednesday":
-            day = 20
-        case "Thursday":
-            day = 30
-        case "Friday":
-            day = 40
-        case "Saturday":
-            day = 50
-        case "Sunday":
-            day = 60
-    # Define a number for each time for calculation
-    match time[:2]:
-        case "00":
-            time = 0
-        case "06":
-            time = 1
-        case "08":
-            time = 2
-        case "10":
-            time = 3
-        case "12":
-            time = 4
-        case "14":
-            time = 5
-        case "16":
-            time = 6
-        case "18":
-            time = 7
-        case "20":
-            time = 8
-        case "22":
-            time = 9
-
-    return time + day
- 
+    set_day = {"Monday": 0, "Tuesday": 10, "Wednesday": 20, "Thursday": 30, "Friday": 40, "Saturday": 50, "Sunday": 60}
+    set_time = {"00": 0, "06": 1, "08": 2, "10": 3, "12": 4, "14": 5, "16": 6, "18": 7, "20": 8, "22": 9}
+    return set_day[day] + set_time[time[:2]]
