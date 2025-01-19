aaa12dce888eec8a4ff062c26b0b38e9f254273a
damianludwiczak89 <damian_konin@tlen.pl>
2025-01-19 21:48:07 +0100

    secret key

--- fitproject/settings.py
+++ fitproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-g*w%bg=ch5$8xl%#jey(%qist&_$)gl28uku_=_+ew6#gt+7-w"
+SECRET_KEY = os.environ['SECRET-KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
